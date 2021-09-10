136814bfa31f1d2e4faa2ab7bdf07e1167203644
Abdul-Malik <82204978+abdul-malik360@users.noreply.github.com>
2021-09-10 14:12:25 +0200

    Update view_client.js

--- scrypts/Client/view_client.js
+++ scrypts/Client/view_client.js
@@ -31,3 +31,28 @@ fetch(
     <p class="username client_details"> Username: ${client[6]}</p>
   </div>`;
   });
+
+// add vehicle
+
+function addVehicleClient() {
+  fetch(`https://qat-motors-api.herokuapp.com/vehicle`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      Type: document.getElementById("client_type").value,
+      Year_Modal: document.getElementById("client_year_modal").value,
+      VIN_Numb: document.getElementById("client_vin_numb").value,
+      Reg_Numb: document.getElementById("client_reg_numb").value,
+      Username: document.getElementById("client_username").value,
+    }),
+  })
+    .then((response) => response.json)
+    .then((data) => {
+      console.log(data);
+      console.log("success");
+      window.location.reload();
+      alert("Vehicle Register Successful, Please Book a Service");
+    });
+}
